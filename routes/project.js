ab9c3f8db0661ff3b8f185a7cff75ba6e27b8064
caohaowen <caohaowen@caohaowen-ThinkPad-X1-Carbon.(none)>
2014-02-19 15:21:57 -0800

    putting it together lab

--- routes/project.js
+++ routes/project.js
@@ -5,6 +5,7 @@ exports.projectInfo = function(req, res) { 
 
   // query for the specific project and
   // call the following callback
+  models.Project.find({"_id":projectID}).exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -18,11 +19,26 @@ exports.addProject = function(req, res) {
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+  var newPost = new models.Project({
+    "title":form_data.project_title,
+    "date":form_data.date,
+    "summary":form_data.summary,
+    "image":form_data.image_url
+  });
+  newPost.save(afterSaving);
+  function afterSaving(err) {
+    if(err) console.log(err);
+    res.send();
+  }
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
-
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+  models.Project.find({"_id" :projectID}).remove().exec(afterRemoving);
+  function afterRemoving(err) {
+    if(err) console.log(err);
+    res.send();
+  }
 }
